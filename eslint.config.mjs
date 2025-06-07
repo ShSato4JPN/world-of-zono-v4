b08a95be7d6e8950e9128a2464bab0d5871140ab
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-06-08 04:09:01 +0900

    feat: add some eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +15,76 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // eslint 対象外のファイルを設定
+    ignores: ["**/.next/**", "**/node_modules/**"],
+  },
+  {
+    // @typescript-eslint の設定（next/typescript に導入済み）
+    files: ["**/*.{js,ts,tsx}"],
+    rules: {
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports" },
+      ],
+      "no-console": "warn",
+      "no-unused-vars": "off",
+      "no-unused-expressions": "warn",
+    },
+  },
+  {
+    // import しているが未使用のパッケージは削除する
+    files: ["**/*.{js,ts,tsx}"],
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  {
+    // import をパッケージ名でソートする
+    files: ["**/*.{js,ts,tsx}"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  {
+    files: ["**/*.{js,ts,tsx}"],
+    plugins: {
+      react: reactPlugin,
+    },
+    rules: {
+      "react/button-has-type": "warn",
+      "react/jsx-child-element-spacing": "warn",
+    },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: { "react-hooks": reactHooks },
+    // ...
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  // prettier 用ルール
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // ビルド時に ESLint のチェックを無視する（デフォルト値）
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
