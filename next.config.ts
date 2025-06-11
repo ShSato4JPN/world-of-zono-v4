962df90317680c7c12bd33803b2c7d4cc33ae127
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-06-11 23:12:24 +0900

    feat: add some packages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+        port: "",
+        pathname: "/t8pgof6z62g7/**",
+        search: "",
+      },
+    ],
+    unoptimized: true,
+  },
   eslint: {
     // ビルド時に ESLint のチェックを無視する（デフォルト値）
     ignoreDuringBuilds: true,
