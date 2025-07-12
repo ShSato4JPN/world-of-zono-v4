32442c30ef01809fc2135084911a782a5013e5e9
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-07-12 17:11:10 +0900

    fix: refactoring header component

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,45 +1,43 @@
 "use client";
-import { motion } from "framer-motion";
-import type { JSX } from "react";
-import { useMemo } from "react";
 
-import { menuList } from "@/app/config/menu";
+import clsx from "clsx";
+import { motion } from "framer-motion";
+import { Tektur } from "next/font/google";
+import Link from "next/link";
+
+import { links } from "@/app/config/page-links";
 import useVerticalScroll from "@/hooks/useVerticalScroll";
 
-import { Button } from "../ui/button";
-import MenuDrawer from "./menu-drawer";
+import MobileMenu from "./mobile-menu";
+import { OnlyMobile, OnlyPc } from "./only-device";
 import ThemeToggle from "./theme-toggle";
+
+const luckiestGuy = Tektur({
+  display: "swap",
+  preload: true,
+  adjustFontFallback: true,
+  fallback: ["sans-serif"],
+  weight: ["500", "700"],
+});
+
+function PageLinks() {
+  return (
+    <div className="h-full flex items-center space-x-10 text-lg font-medium">
+      {links.map(({ label, path }) => (
+        <Link key={label} href={path}>
+          {label}
+        </Link>
+      ))}
+    </div>
+  );
+}
 
 export default function Header() {
   const { isOver } = useVerticalScroll({ threshold: 200 });
 
-  const menuItems = useMemo<JSX.Element>(
-    () => (
-      <ul className="w-[450px] flex items-center justify-around">
-        {menuList.map((item) => (
-          <li key={item.path} className="flex-1">
-            <div className="flex flex-col items-center">
-              <Button variant="ghost" size="icon">
-                <item.icon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-              </Button>
-              <span className="text-sm">{item.label}</span>
-            </div>
-          </li>
-        ))}
-        <li className="flex-1">
-          <div className="flex flex-col items-center">
-            <ThemeToggle />
-            <span className="text-sm">theme</span>
-          </div>
-        </li>
-      </ul>
-    ),
-    [],
-  );
-
   return (
     <motion.header
-      className="fixed grid place-items-center top-0 left-0 right-0 z-50 p-1.5 sm:p-0 bg-background/80 backdrop-blur shadow"
+      className={clsx(luckiestGuy.className, "font-bold")}
       variants={{
         visible: {
           y: 0,
@@ -61,20 +59,32 @@ export default function Header() {
       initial="visible"
       animate={isOver ? "hidden" : "visible"}
     >
-      <div className="w-full max-w-[1300px] grid grid-cols-[auto_1fr_auto] sm:grid-cols-1">
-        <div className="grid place-items-center sm:hidden">
-          <MenuDrawer direction="right" />
-        </div>
-        <div className="flex flex-row items-center justify-center sm:justify-around">
-          <h1 className="font-bold text-center text-xl sm:text-4xl">
-            World of Zono
-          </h1>
-          <nav className="hidden sm:block sm:p-1">{menuItems}</nav>
-        </div>
-        <div className="grid place-items-center sm:hidden">
-          <ThemeToggle />
-        </div>
-      </div>
+      <nav>
+        <OnlyMobile
+          element={
+            <div className="flex items-center justify-between p-1.5">
+              <MobileMenu />
+              <Link href="/">
+                <span className="text-xl sm:text-4xl">World-Of-Zono</span>
+              </Link>
+              <ThemeToggle />
+            </div>
+          }
+        />
+        <OnlyPc
+          element={
+            <div className="flex items-center justify-between p-3">
+              <div className="flex items-center space-x-10">
+                <Link href="/">
+                  <span className="text-xl sm:text-4xl">World-Of-Zono</span>
+                </Link>
+                <PageLinks />
+              </div>
+              <ThemeToggle />
+            </div>
+          }
+        />
+      </nav>
     </motion.header>
   );
 }
